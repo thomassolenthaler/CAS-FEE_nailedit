e0a0952926927d790a7cb6f392b94c778cf2cc68
Thomas Solenthaler <tsolenthaler@gmail.com>
2014-06-26 10:32:30 +0200

    Add Content from old Project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+    $('.btn-share').on('click',function() {
+        var content = $('#input-article').val();
+        nailit.addEntry(content);
+
+        return false;
+    });
+
+
+});
+
+var nailit = {
+    addEntry:function(content) {
+        var $article = $('<article></article>');
+        $article.append('<img />').append('<h1>'+content+'</h1>').append('<p></p>');
+        $('.article-wrapper').append($article);
+    }
+};
